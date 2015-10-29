42fd8da4589b4293137ed3e90acb05309eed3da2
wildanmuslim <wildan16muslim@gmail.com>
2015-10-29 23:40:33 +0700

    first commit

---
+++ OwlResto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OwlResto.Startup))]
+namespace OwlResto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
